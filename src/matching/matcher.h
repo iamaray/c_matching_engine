14f7047be0df5c9dd988c86c9f56ef93d7410358
iamaray <arya.nahri@gmail.com>
2025-03-12 12:36:32 -0400

    matching header

--- src/matching/matcher.h
+++ src/matching/matcher.h
@@ -5,8 +5,22 @@
 #include <stdlib.h>
 #include <limits.h>
 #include <string.h>
+#include "order.h"
 #include "orderbook.h"
+#include "orderheap.h"
+#include "ordermap.h"
 
+/*
+    Matching engine logic
+*/
+
+// int _fill_trade(
+//     Order *maker,
+//     Order *taker,
+//     OrderHeap *buy_orders,
+//     OrderHeap *sell_orders);
+
+int match_orderbook(OrderBook *book);
 
 
 #endif
